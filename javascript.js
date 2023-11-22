5ec9d16972c28ebca086e28a8323945eec831ef3
Guepardeau <146670042+Guepardeau@users.noreply.github.com.>
2023-11-22 11:13:17 +0000

    add loop and gameEnd(); fully functioning

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
-let randomNumber = Math.floor(Math.random()* 3)+ 1;
-let playerSelection = prompt("Please choose Rock, Paper or Scissors!").toLowerCase();
+let randomNumber;
 let compSelection;
+let playerSelection;
+let playerScore = 0;
+let compScore = 0;
 
 function getComputerChoice() {
-
     if (randomNumber === 1) {
         return compSelection = "rock";
     } else if (randomNumber === 2) {
@@ -13,31 +14,72 @@ function getComputerChoice() {
     }
     };  
 
-function singleround() {
-   
-    if (playerSelection === "rock" && compSelection === "paper"){
-        console.log("Oh No! You lost, Paper beats Rock :(");
-    } else if (playerSelection === "rock" && compSelection === "scissors") {
-        console.log("Well done! You won, Rock beats Scissors!");
-    } else if (playerSelection === "paper" && compSelection === "rock") {
-        console.log("Well done! You won, Paper beats Rock!");
-    } else if (playerSelection === "paper" && compSelection === "scissors") {
-        console.log("Oh No! You lost, Scissors beats Paper :(");
-    } else if (playerSelection === "scissors" && compSelection === "rock") {
-        console.log("Oh No! You lost, Rock beats Scissors :(");
-    } else if (playerSelection === "scissors" && compSelection === "paper") {
-        console.log("Well done! You won, Scissors beats Paper!");
-    } else if (playerSelection === compSelection) {
-        console.log("It's a tie! Try again.");
+function game() {
+   for (let i=0; i <= 4; i++) {
+
+        randomNumber = Math.floor(Math.random()* 3)+ 1;
+        getComputerChoice();
+        playerSelection = prompt("Please choose Rock, Paper or Scissors!").toLowerCase();
+        console.log(`You: ${playerScore} Computer ${compScore}`);
+
+        if (playerSelection === "rock" && compSelection === "paper"){
+                console.log("Oh No! You lost, Paper beats Rock :(");
+                return playerLose();
+            } else if (playerSelection === "rock" && compSelection === "scissors") {
+                console.log("Well done! You won, Rock beats Scissors!");
+                return playerWin();
+            } else if (playerSelection === "paper" && compSelection === "rock") {
+                console.log("Well done! You won, Paper beats Rock!");
+                return playerWin();
+            } else if (playerSelection === "paper" && compSelection === "scissors") {
+                console.log("Oh No! You lost, Scissors beats Paper :(");
+                return playerLose(); 
+            } else if (playerSelection === "scissors" && compSelection === "rock") {
+                console.log("Oh No! You lost, Rock beats Scissors :(");
+                return playerLose();
+            } else if (playerSelection === "scissors" && compSelection === "paper") {
+                console.log("Well done! You won, Scissors beats Paper!");
+                return playerWin();
+            } else if (playerSelection === compSelection) {
+                console.log("It's a tie! Try again.");
+            } else {
+                console.log("Uh Oh! Something's gone wrong. Refresh the page to try again!");
+            }  
+            }
+
+            function playerLose () {
+                ++compScore;
+                if (compScore || playerScore === 5) {
+                    gameEnd();
+                } else {
+                    game();
+                }
+            }
+            
+            function playerWin () {
+                ++playerScore;
+                if (compScore || playerScore === 5) {
+                    gameEnd();
+                } else {
+                    game();
+                }
+            }
+
+        };
+
+game();
+
+
+
+function gameEnd() {
+    console.log(`You: ${playerScore} Computer ${compScore}`);
+    if (playerScore === 5){
+        console.log("You won!!! Refresh to play again.");
+        return
+    } else if (compScore === 5){
+        console.log("Oh no :( You lost. Refresh to try again.");
+        return
     } else {
-        console.log("Uh Oh! Something's gone wrong. Refresh the page to try again!");
-    }  
-    };
-
-getComputerChoice(compSelection);
-
-
-console.log(compSelection);
-console.log(playerSelection);
-
-singleround(playerSelection,compSelection);
+        game();
+    }
+}
